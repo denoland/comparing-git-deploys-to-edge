504788d1527684de78ae84b6521a200a119d6e9e
lambtron <andyjiang@gmail.com>
2023-01-23 19:02:46 -0800

    update

--- vercel/api/hello.ts
+++ vercel/api/hello.ts
@@ -2,4 +2,4 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-export default (req) => new Response('Hello world from hazelthenuttypug!');
+export default (req) => new Response('Hello world from andyjiang!');
